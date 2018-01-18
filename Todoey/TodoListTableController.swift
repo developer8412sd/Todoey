a8821848f7cb1b3704b28ef6c62075f707683255
vkapranov <vkapranov@yahoo.com>
2018-01-18 18:33:43 -0500

    To do list complete without data persistence

---
+++ Todoey/TodoListTableController.swift
@@ -0,0 +1,38 @@
+//
+//  TodoListTableController.swift
+//  Todoey
+//
+//  Created by Valentin Kapranov on 1/18/18.
+//  Copyright © 2018 Valentin Kapranov. All rights reserved.
+//
+
+import UIKit
+
+class TodoListTableController: UITableViewController {
+
+    let arryVlaues = ["Row 1","Row 2","Row 3"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arryVlaues.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoListCell", for: indexPath)
+        cell.textLabel?.text = arryVlaues[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
